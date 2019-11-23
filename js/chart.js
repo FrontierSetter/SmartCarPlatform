4d115e8e17867498c0f6e645597592909759df03
Lu Yihui <lu_yihui@126.com>
2019-11-23 15:59:38 +0800

    small charts finish

--- js/chart.js
+++ js/chart.js
@@ -42,7 +42,7 @@ option2 = {
         },
         axisLine:{
             lineStyle:{
-                color: '#63B8FF'
+                color: '#999'
             }
         }
     },
@@ -54,7 +54,7 @@ option2 = {
         },
         axisLine:{
             lineStyle:{
-                color: '#63B8FF'
+                color: '#999'
             }
         }
     },
@@ -88,6 +88,13 @@ option3 = {
         trigger: 'axis',
         formatter:'{b1}: {c1}'
     },
+    grid: {
+        top: '5%',
+        left: '3%',
+        right: '4%',
+        bottom: '8%',
+        containLabel: true
+    },
     xAxis: {
         data: dataAxis3,
         axisLabel: {
@@ -100,13 +107,19 @@ option3 = {
             show: false
         },
         axisLine: {
-            show: false
+            show: false,
+            lineStyle:{
+                color: '#63B8FF'
+            }
         },
         z: 10
     },
     yAxis: {
         axisLine: {
-            show: false
+            show: false,
+            lineStyle:{
+                color: '#63B8FF'
+            }
         },
         axisTick: {
             show: false
@@ -164,3 +177,501 @@ option3 = {
 
 
 //////////////////////品牌实时热度 end
+
+// 车型投诉量走势
+data = [["2000-06-05",116],["2000-06-06",129],["2000-06-07",135],["2000-06-08",86],["2000-06-09",73],["2000-06-10",85],["2000-06-11",73],["2000-06-12",68],["2000-06-13",92],["2000-06-14",130],["2000-06-15",245],["2000-06-16",139],["2000-06-17",115],["2000-06-18",111],["2000-06-19",309],["2000-06-20",206],["2000-06-21",137],["2000-06-22",128],["2000-06-23",85],["2000-06-24",94],["2000-06-25",71],["2000-06-26",106],["2000-06-27",84],["2000-06-28",93],["2000-06-29",85],["2000-06-30",73],["2000-07-01",83],["2000-07-02",125],["2000-07-03",107],["2000-07-04",82],["2000-07-05",44],["2000-07-06",72],["2000-07-07",106],["2000-07-08",107],["2000-07-09",66],["2000-07-10",91],["2000-07-11",92],["2000-07-12",113],["2000-07-13",107],["2000-07-14",131],["2000-07-15",111],["2000-07-16",64],["2000-07-17",69],["2000-07-18",88],["2000-07-19",77],["2000-07-20",83],["2000-07-21",111],["2000-07-22",57],["2000-07-23",55],["2000-07-24",60]];
+var valueList = data.map(function (item) {
+    return item[1];
+});
+var dateList = data.map(function (item) {
+    return item[0];
+});
+option5 = {
+
+    // Make gradient line here
+    visualMap: {
+        show: false,
+        type: 'continuous',
+        seriesIndex: 0,
+        min: 0,
+        max: 400
+    },
+
+
+    // title: [{
+    //     left: 'center',
+    //     text: 'Gradient along the y axis'
+    // }],
+    tooltip: {
+        trigger: 'axis'
+    },
+    xAxis: {
+        data: dateList,
+        axisLabel: {
+            textStyle: {
+                color: '#999'
+            }
+        }
+    },
+    yAxis: {
+        splitLine: {show: false},
+        axisLabel: {
+            textStyle: {
+                color: '#999'
+            }
+        }
+    },
+    series: {
+        type: 'line',
+        showSymbol: false,
+        data: valueList
+    }
+};
+/////////////车型投诉量走势
+
+// 百车故障数
+
+option6 = {
+    color: ['#3398DB'],
+    tooltip : {
+        trigger: 'axis',
+        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    grid: {
+        top: '5%',
+        left: '3%',
+        right: '4%',
+        bottom: '8%',
+        containLabel: true
+    },
+    xAxis : [
+        {
+            type : 'category',
+            data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            axisTick: {
+                alignWithLabel: true
+            },
+            axisLine: {
+                lineStyle:{
+                    color: '#999'
+                }
+            },
+        }
+    ],
+    yAxis : [
+        {
+            type : 'value',
+            axisLine: {
+                lineStyle:{
+                    color: '#999'
+                }
+            },
+        }
+    ],
+    series : [
+        {
+            name:'直接访问',
+            type:'bar',
+            barWidth: '60%',
+            data:[10, 52, 200, 334, 390, 330, 220]
+        }
+    ]
+};
+
+///////////////////////百车故障数
+
+// 车型评分雷达图
+var labels7 = ['1', '2', '3', '4', '5', '6'];
+option7 = {
+    // title: {
+    //     text: '基础雷达图'
+    // },
+    tooltip: {},
+    legend: {
+        data: ['均值', '当前'],
+        orient: 'vertical',
+        x:'left',
+        textStyle:{//图例文字的样式
+            color:'#63B8FF',
+        }
+    },
+    radar: {
+        // shape: 'circle',
+        radius:'50%',
+        center:['50%', '40%'],
+        name: {
+            textStyle: {
+                color: '#fff',
+                backgroundColor: '#999',
+                borderRadius: 3,
+                padding: [3, 5]
+           }
+        },
+        indicator: [
+           { name: '销售', max: 6500},
+           { name: '管理', max: 16000},
+           { name: '技术', max: 30000},
+           { name: '客服', max: 38000},
+           { name: '研发', max: 52000},
+           { name: '市场', max: 25000}
+        ]
+    },
+    series: [{
+        name: '当前 vs 均值',
+        type: 'radar',
+        // radius: '50%',
+        // areaStyle: {normal: {}},
+        data : [
+            {
+                value : [5000, 14000, 28000, 31000, 42000, 21000],
+                name : '均值',
+                lineStyle: {
+                    normal: {
+                        type: 'dashed'
+                    }
+                }
+            },
+            {
+                value : [4300, 10000, 28000, 35000, 50000, 19000],
+                name : '当前',
+                areaStyle: {
+                    normal: {
+                        opacity: 0.9,
+                        color: new echarts.graphic.RadialGradient(0.5, 0.5, 1, [
+                            {
+                                color: '#B8D3E4',
+                                offset: 0
+                            },
+                            {
+                                color: '#72ACD1',
+                                offset: 1
+                            }
+                        ])
+                    }
+                }
+            }
+        ]
+    }]
+};
+/////////////////////车型评分雷达图
+
+// 当月评论情感分析
+
+option8 = {
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)"
+    },
+    legend: {
+        type: 'scroll',
+        orient: 'horizontal',
+        x: 'left',
+        data:['直达','营销广告','搜索引擎','邮件营销','联盟广告','视频广告','百度','谷歌','必应','其他'],
+        textStyle:{//图例文字的样式
+            color:'#63B8FF',
+        }
+    },
+    series: [
+        {
+            name:'访问来源',
+            type:'pie',
+            selectedMode: 'single',
+            radius: [0, '30%'],
+
+            label: {
+                normal: {
+                    position: 'inner'
+                },
+                textStyle:{
+                    fontSize : 1
+                }
+            },
+            labelLine: {
+                normal: {
+                    show: false
+                }
+            },
+            data:[
+                {value:335, name:'直达', selected:true},
+                {value:679, name:'营销广告'},
+                {value:1548, name:'搜索引擎'}
+            ]
+        },
+        {
+            name:'访问来源',
+            type:'pie',
+            radius: ['40%', '55%'],
+            lanel:{
+                show:'false'
+            },
+            label: {
+                normal: {
+                    show: false,
+                    // position: 'center'
+                },
+                emphasis: {
+                    show: true,
+                    textStyle: {
+                        fontSize: '15',
+                        fontWeight: 'bold'
+                    }
+                }
+            },
+            // label: {
+            //     normal: {
+            //         formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
+            //         backgroundColor: '#eee',
+            //         borderColor: '#aaa',
+            //         borderWidth: 1,
+            //         borderRadius: 4,
+            //         // shadowBlur:3,
+            //         // shadowOffsetX: 2,
+            //         // shadowOffsetY: 2,
+            //         // shadowColor: '#999',
+            //         // padding: [0, 7],
+            //         rich: {
+            //             a: {
+            //                 color: '#999',
+            //                 lineHeight: 22,
+            //                 align: 'center'
+            //             },
+            //             // abg: {
+            //             //     backgroundColor: '#333',
+            //             //     width: '100%',
+            //             //     align: 'right',
+            //             //     height: 22,
+            //             //     borderRadius: [4, 4, 0, 0]
+            //             // },
+            //             hr: {
+            //                 borderColor: '#aaa',
+            //                 width: '100%',
+            //                 borderWidth: 0.5,
+            //                 height: 0
+            //             },
+            //             b: {
+            //                 fontSize: 16,
+            //                 lineHeight: 33
+            //             },
+            //             per: {
+            //                 color: '#eee',
+            //                 backgroundColor: '#334455',
+            //                 padding: [2, 4],
+            //                 borderRadius: 2
+            //             }
+            //         }
+            //     }
+            // },
+            data:[
+                {value:335, name:'直达'},
+                {value:310, name:'邮件营销'},
+                {value:234, name:'联盟广告'},
+                {value:135, name:'视频广告'},
+                {value:1048, name:'百度'},
+                {value:251, name:'谷歌'},
+                {value:147, name:'必应'},
+                {value:102, name:'其他'}
+            ]
+        }
+    ]
+};;
+
+/////////////////////当月评论情感分析
+
+// 品牌车辆销售占比
+option9 = {
+
+    // title: {
+    //     text: 'Customized Pie',
+    //     left: 'center',
+    //     top: 20,
+    //     textStyle: {
+    //         color: '#ccc'
+    //     }
+    // },
+
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+
+    visualMap: {
+        show: false,
+        min: 80,
+        max: 600,
+        inRange: {
+            colorLightness: [0, 1]
+        }
+    },
+    legend: {
+        type: 'scroll',
+        orient: 'horizontal',
+        // orient: 'vertical',
+        left: 'left',
+        data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎'],
+        textStyle:{//图例文字的样式
+            color:'#63B8FF',
+        }
+    },
+    series : [
+        {
+            name:'访问来源',
+            type:'pie',
+            radius : '55%',
+            center: ['50%', '50%'],
+            data:[
+                {value:335, name:'直接访问'},
+                {value:310, name:'邮件营销'},
+                {value:274, name:'联盟广告'},
+                {value:235, name:'视频广告'},
+                {value:400, name:'搜索引擎'}
+            ].sort(function (a, b) { return a.value - b.value; }),
+            roseType: 'radius',
+            label: {
+                normal: {
+                    textStyle:{//图例文字的样式
+                        color:'#63B8FF',
+                    }
+                }
+            },
+            labelLine: {
+                normal: {
+                    lineStyle: {
+                        color: 'rgba(255, 255, 255, 0.3)'
+                    },
+                    smooth: 0.2,
+                    length: 10,
+                    length2: 20
+                }
+            },
+            itemStyle: {
+                normal: {
+                    // color: '#c23531',
+                    shadowBlur: 200,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            },
+
+            animationType: 'scale',
+            animationEasing: 'elasticOut',
+            animationDelay: function (idx) {
+                return Math.random() * 200;
+            }
+        }
+    ]
+};;
+/////////////////////品牌车辆销售占比
+
+// 购车目的
+option10 = {
+    // title : {
+    //     text: '某站点用户访问来源',
+    //     subtext: '纯属虚构',
+    //     x:'center'
+    // },
+    // grid:{
+    //     top:'1%',
+    // },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        type: 'scroll',
+        orient: 'horizontal',
+        // orient: 'vertical',
+        left: 'left',
+        data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎'],
+        textStyle:{//图例文字的样式
+            color:'#63B8FF',
+        }
+    },
+    series : [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius : '55%',
+            center: ['50%', '50%'],
+            label: {
+                normal: {
+                    show: false,
+                    // position: 'center'
+                },
+                emphasis: {
+                    show: true,
+                    textStyle: {
+                        fontSize: '15',
+                        fontWeight: 'bold'
+                    }
+                }
+            },
+            data:[
+                {value:335, name:'直接访问'},
+                {value:310, name:'邮件营销'},
+                {value:234, name:'联盟广告'},
+                {value:135, name:'视频广告'},
+                {value:1548, name:'搜索引擎'}
+            ],
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    ]
+};
+//////////////////// 购车目的
+
+// 百车故障分析
+option13 = {
+    tooltip: {
+        trigger: 'item',
+        formatter: "{a} <br/>{b}: {c} ({d}%)"
+    },
+    legend: {
+        type: 'scroll',
+        orient: 'horizontal',
+        x: 'left',
+        data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎'],
+        textStyle:{//图例文字的样式
+            color:'#63B8FF',
+        }
+    },
+    series: [
+        {
+            name:'访问来源',
+            type:'pie',
+            radius: ['40%', '60%'],
+            avoidLabelOverlap: false,
+            label: {
+                normal: {
+                    show: false,
+                    position: 'center'
+                },
+                emphasis: {
+                    show: true,
+                    textStyle: {
+                        fontSize: '16',
+                        fontWeight: 'bold'
+                    }
+                }
+            },
+            labelLine: {
+                normal: {
+                    show: false
+                }
+            },
+            data:[
+                {value:335, name:'直接访问'},
+                {value:310, name:'邮件营销'},
+                {value:234, name:'联盟广告'},
+                {value:135, name:'视频广告'},
+                {value:1548, name:'搜索引擎'}
+            ]
+        }
+    ]
+};
+//////////////百车故障分析

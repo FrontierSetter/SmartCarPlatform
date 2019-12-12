d0e792501432af8f6daff6eb8c0ecd9e8089ce3b
Lu Yihui <lu_yihui@126.com>
2019-12-12 22:49:04 +0800

    sales data

--- public/js/chart.js
+++ public/js/chart.js
@@ -30,44 +30,49 @@ option2 = {
             return [pt[0], '10%'];
         }
     },
+    grid: {
+        top: '7%',
+        left: '3%',
+        right: '4%',
+        bottom: '8%',
+        containLabel: true
+    },
     // title: {
     //     left: 'center',
     //     text: '大数据量面积图',
     // },
-    toolbox: {
-        feature: {
-            dataZoom: {
-                yAxisIndex: 'none'
-            },
-            restore: {},
-            saveAsImage: {}
-        }
-    },
+    // toolbox: {
+    //     feature: {
+    //         dataZoom: {
+    //             yAxisIndex: 'none'
+    //         },
+    //         restore: {},
+    //         saveAsImage: {}
+    //     }
+    // },
     xAxis: {
         type: 'category',
         boundaryGap: false,
-        data: date2
+        data: date2,
+        axisLabel:{
+            textStyle: {
+                color: '#fff'
+            },
+        }
     },
     yAxis: {
         type: 'value',
-        boundaryGap: [0, '100%']
+        boundaryGap: [0, '100%'],
+        axisLabel: {
+            textStyle: {
+                color: '#999'
+            }
+        }
     },
     dataZoom: [{
         type: 'inside',
         start: 0,
-        end: 10
-    }, {
-        start: 0,
-        end: 10,
-        handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
-        handleSize: '80%',
-        handleStyle: {
-            color: '#fff',
-            shadowBlur: 3,
-            shadowColor: 'rgba(0, 0, 0, 0.6)',
-            shadowOffsetX: 2,
-            shadowOffsetY: 2
-        }
+        end: 100
     }],
     series: [
         {
@@ -185,82 +190,82 @@ option3 = {
             },
             interval:0,
             // rotate:45,
-            rich: {
-                value: {
-                    lineHeight: 30,
-                    align: 'center'
-                },
-                奥迪: {
-                    height: 40,
-                    align: 'center',
-                    backgroundColor: {
-                        image: 'images/car_brand/奥迪.jpg'
-                    }
-                },
-                宝马: {
-                    height: 40,
-                    align: 'center',
-                    backgroundColor: {
-                        image: 'images/car_brand/宝马.jpg'
-                    }
-                },
-                保时捷: {
-                    height: 40,
-                    align: 'center',
-                    backgroundColor: {
-                        image: 'images/car_brand/保时捷.jpg'
-                    }
-                },
-                奔驰: {
-                    height: 40,
-                    align: 'center',
-                    backgroundColor: {
-                        image: 'images/car_brand/奔驰.jpg'
-                    }
-                },
-                本田: {
-                    height: 40,
-                    align: 'center',
-                    backgroundColor: {
-                        image: 'images/car_brand/本田.jpg'
-                    }
-                },
-                比亚迪: {
-                    height: 40,
-                    align: 'center',
-                    backgroundColor: {
-                        image: 'images/car_brand/比亚迪.jpg'
-                    }
-                },
-                大众: {
-                    height: 40,
-                    align: 'center',
-                    backgroundColor: {
-                        image: 'images/car_brand/大众.jpg'
-                    }
-                },
-                丰田: {
-                    height: 40,
-                    align: 'center',
-                    backgroundColor: {
-                        image: 'images/car_brand/丰田.jpg'
-                    }
-                },
-                吉利: {
-                    height: 40,
-                    align: 'center',
-                    backgroundColor: {
-                        image: 'images/car_brand/吉利.jpg'
-                    }
-                },
-                特斯拉: {
-                    height: 40,
-                    align: 'center',
-                    backgroundColor: {
-                        image: 'images/car_brand/特斯拉.jpg'
-                    }
-                }
-            }
+            // rich: {
+            //     value: {
+            //         lineHeight: 30,
+            //         align: 'center'
+            //     },
+            //     奥迪: {
+            //         height: 40,
+            //         align: 'center',
+            //         backgroundColor: {
+            //             image: 'images/car_brand/奥迪.jpg'
+            //         }
+            //     },
+            //     宝马: {
+            //         height: 40,
+            //         align: 'center',
+            //         backgroundColor: {
+            //             image: 'images/car_brand/宝马.jpg'
+            //         }
+            //     },
+            //     保时捷: {
+            //         height: 40,
+            //         align: 'center',
+            //         backgroundColor: {
+            //             image: 'images/car_brand/保时捷.jpg'
+            //         }
+            //     },
+            //     奔驰: {
+            //         height: 40,
+            //         align: 'center',
+            //         backgroundColor: {
+            //             image: 'images/car_brand/奔驰.jpg'
+            //         }
+            //     },
+            //     本田: {
+            //         height: 40,
+            //         align: 'center',
+            //         backgroundColor: {
+            //             image: 'images/car_brand/本田.jpg'
+            //         }
+            //     },
+            //     比亚迪: {
+            //         height: 40,
+            //         align: 'center',
+            //         backgroundColor: {
+            //             image: 'images/car_brand/比亚迪.jpg'
+            //         }
+            //     },
+            //     大众: {
+            //         height: 40,
+            //         align: 'center',
+            //         backgroundColor: {
+            //             image: 'images/car_brand/大众.jpg'
+            //         }
+            //     },
+            //     丰田: {
+            //         height: 40,
+            //         align: 'center',
+            //         backgroundColor: {
+            //             image: 'images/car_brand/丰田.jpg'
+            //         }
+            //     },
+            //     吉利: {
+            //         height: 40,
+            //         align: 'center',
+            //         backgroundColor: {
+            //             image: 'images/car_brand/吉利.jpg'
+            //         }
+            //     },
+            //     特斯拉: {
+            //         height: 40,
+            //         align: 'center',
+            //         backgroundColor: {
+            //             image: 'images/car_brand/特斯拉.jpg'
+            //         }
+            //     }
+            // }
         },
         axisTick: {
             show: false
